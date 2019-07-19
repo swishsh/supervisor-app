45192868f83c0329333bb86c4b78bd5ec54ca8c8
stefan.voinea <stefan.voinea@emag.ro>
2019-07-19 18:12:32 +0300

    small fix

--- src/Controller/SupervisorController.php
+++ src/Controller/SupervisorController.php
@@ -2,16 +2,9 @@
 
 namespace App\Controller;
 
-use App\Dto\Consumer;
 use App\FormType\ConsumerCollectionType;
 use App\Service\Read\ReadSupervisorYaml;
-use function Sodium\add;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Tests\Extension\DependencyInjection\TestTypeExtension;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SupervisorController extends AbstractController
 {
@@ -28,37 +21,11 @@ class SupervisorController extends AbstractController
         error_reporting(0);
 
         $consumerCollection = $this->readSupervisorYaml->readConsumers();
-//        $forms = [];
-//
-//        foreach ($consumerCollection->getConsumers() as $consumer) {
-//            $form = $this->createFormBuilder($consumer)
-//                ->add('name', TextType::class)
-//                ->add('template', TextType::class)
-//                ->add('command', TextType::class)
-//                ->add('platform', TextType::class)
-//                ->add('numprocs', TextType::class)
-//                ->add('autostart', TextType::class)
-//                ->add('startsecs', TextType::class)
-//                ->add('ignore', TextType::class)
-//                ->add('stopwaitsecs', TextType::class)
-//                ->add('save', SubmitType::class, ['label' => 'Save supervisor config'])
-//                ->getForm()->createView();
-//
-//            $forms[] = $form;
-//        }
 
         $form = $this->createForm(ConsumerCollectionType::class, $consumerCollection);
 
         return $this->render('read_supervisor.html.twig', [
             'form' => $form->createView()
         ]);
-
-
-
-
-        var_dump($consumers);
-        die('asdasdasd');
-
-        return new JsonResponse(array($number));
     }
 }
--- src/FormType/ConsumerType.php
+++ src/FormType/ConsumerType.php
@@ -6,7 +6,6 @@ use App\Dto\Consumer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
